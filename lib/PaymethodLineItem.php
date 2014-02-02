117a3e3e508a8c2e72db3833173879137fb4f3e9
Matthias Weiss <matthias@more-onion.com>
2014-02-02 18:27:56 +0100

    Improved UI for line item editing.

--- lib/PaymethodLineItem.php
+++ lib/PaymethodLineItem.php
@@ -9,6 +9,6 @@ namespace Drupal\webform_paymethod_select;
  *
  */
 class PaymethodLineItem extends \PaymentLineItem {
-  public $component_or_fixed = 'fixed';
-  public $amount_component   = NULL;
+  public $amount_source    = 'fixed';
+  public $amount_component = NULL;
 }
