133a0796e6b466c2e760b2087bc64b7f259c4ac7
Matthias Weiss <matthias@more-onion.com>
2014-09-19 17:14:01 +0200

    [PaymethodLineItem] Added export() method for features integration

--- lib/PaymethodLineItem.php
+++ lib/PaymethodLineItem.php
@@ -11,4 +11,9 @@ namespace Drupal\webform_paymethod_select;
 class PaymethodLineItem extends \PaymentLineItem {
   public $amount_source    = 'fixed';
   public $amount_component = NULL;
+
+  public function export() {
+    $serialized = serialize($this);
+    return "unserialize('$serialized')";
+  }
 }
